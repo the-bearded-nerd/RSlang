9a3b681ec72d9d2209bf6b0750398a561927d363
Anna <Laluna-93@yandex.ru>
2022-08-26 00:41:49 +0400

    fix: fixed the functionality of displaying the active difficulty level

--- src/components/textbook/HeaderTextBook/HeaderTextbook.tsx
+++ src/components/textbook/HeaderTextBook/HeaderTextbook.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import { TypeSection } from '../../interface/interfaces';
 import './menuTextBook.css';
 
-function HeaderTextbook({ typeSection, setIdHard, setNumberPage }: TypeSection) {
-  const [item, newItem] = React.useState('Первый');
+function HeaderTextbook({ typeSection, setIdHard, setNumberPage, idHard }: TypeSection) {
   const numberHard = [0, 1, 2, 3, 4, 5, 6];
   return (
     <ul className="menu">
       {typeSection.map((e, i) => (
         <button
           type="button"
-          className={item === e ? `typeSection active` : 'typeSection'}
+          className={idHard === i ? `typeSection active` : 'typeSection'}
           onClick={() => {
-            newItem(e);
             setIdHard(numberHard[i]);
             setNumberPage();
           }}
