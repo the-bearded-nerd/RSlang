806abf2695f950757826100b35547845b8c142f2
Anna <Laluna-93@yandex.ru>
2022-08-30 21:34:13 +0400

    feat: added page status update when switching to another page

--- src/components/textbook/BtnPagination/BtnPageTextBook.tsx
+++ src/components/textbook/BtnPagination/BtnPageTextBook.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import UserAggregatedWords from '../../../utils/UsersAggregatedWords/UserAggregatedWords';
 import { PropsBtnPage } from '../../interface/interfaces';
 
-export function BtnNextPage({ setNumberPage, currentCount }: PropsBtnPage) {
+export function BtnNextPage({
+  setNumberPage,
+  currentCount,
+  currentWords,
+  isLearnPage,
+}: PropsBtnPage) {
   return (
     <button
       className="Btn-Word"
       disabled={currentCount === 30}
       type="button"
       onClick={() => {
+        UserAggregatedWords.allLearned(currentWords)
+          .then((e) => e)
+          .then((i) => isLearnPage(i));
         setNumberPage();
       }}
     >
@@ -16,13 +25,21 @@ export function BtnNextPage({ setNumberPage, currentCount }: PropsBtnPage) {
   );
 }
 
-export function BtnPrevPage({ setNumberPage, currentCount }: PropsBtnPage) {
+export function BtnPrevPage({
+  setNumberPage,
+  currentCount,
+  currentWords,
+  isLearnPage,
+}: PropsBtnPage) {
   return (
     <button
       className="Btn-Word"
       disabled={currentCount === 1}
       type="button"
       onClick={() => {
+        UserAggregatedWords.allLearned(currentWords)
+          .then((e) => e)
+          .then((i) => isLearnPage(i));
         setNumberPage();
       }}
     >
