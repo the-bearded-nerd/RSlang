b8c8c13cb81fd6490b816aa7a289c7c4b04749e2
Anna <Laluna-93@yandex.ru>
2022-09-04 07:47:03 +0400

    feat: added text if the word has no progress

--- src/components/textbook/StudyProgress/StudyProgress.tsx
+++ src/components/textbook/StudyProgress/StudyProgress.tsx
@@ -15,26 +15,29 @@ function StudyProgress({ classStudy, isClassStudy, word }: ClassStudy) {
         Закрыть
       </button>
       <h2 className="buttonStudy">{word?.word}</h2>
-
-      <table className="table">
-        <tbody>
-          <tr>
-            <td />
-            <td>Верно выбрано</td>
-            <td>Ошибка</td>
-          </tr>
-          <tr>
-            <td>Спринт</td>
-            <td>{word?.userWord.optional?.sprint.right}</td>
-            <td>{word?.userWord.optional?.sprint.wrong}</td>
-          </tr>
-          <tr>
-            <td>Аудиовызов</td>
-            <td>{word?.userWord.optional?.audio.right}</td>
-            <td>{word?.userWord.optional?.audio.wrong}</td>
-          </tr>
-        </tbody>
-      </table>
+      {word?.userWord === undefined ? (
+        <h1>Прогресса изучения слова {word?.word} нет </h1>
+      ) : (
+        <table className="table">
+          <tbody>
+            <tr>
+              <td />
+              <td>Верно выбрано</td>
+              <td>Ошибка</td>
+            </tr>
+            <tr>
+              <td>Спринт</td>
+              <td>{word?.userWord.optional?.sprint.right}</td>
+              <td>{word?.userWord.optional?.sprint.wrong}</td>
+            </tr>
+            <tr>
+              <td>Аудиовызов</td>
+              <td>{word?.userWord.optional?.audio.right}</td>
+              <td>{word?.userWord.optional?.audio.wrong}</td>
+            </tr>
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
