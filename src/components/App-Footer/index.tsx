39ecdee36ca9c6cd8ccab699e44b928074f851bd
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2022-09-06 01:17:55 +0300

    fix: fix header layout

--- src/components/App-Footer/index.tsx
+++ src/components/App-Footer/index.tsx
@@ -5,27 +5,29 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="container">
-        <div className="footer_git">
-          <a href="https://github.com/the-bearded-nerd">
-            <img src="../../../logoGithub.svg" width="50" height="50" alt="logo.svg" />
-          </a>
-          <a href="https://github.com/bloodsuckers-spb">
-            <img src="../../../logoGithub.svg" width="50" height="50" alt="logo.svg" />
-          </a>
-          <a href="https://github.com/laluna93">
-            <img src="../../../logoGithub.svg" width="50" height="50" alt="logo.svg" />
+        <div className="footer-inner">
+          <div className="footer_git">
+            <a href="https://github.com/the-bearded-nerd">
+              <img src="../../../logoGithub.svg" width="50" height="50" alt="logo.svg" />
+            </a>
+            <a href="https://github.com/bloodsuckers-spb">
+              <img src="../../../logoGithub.svg" width="50" height="50" alt="logo.svg" />
+            </a>
+            <a href="https://github.com/laluna93">
+              <img src="../../../logoGithub.svg" width="50" height="50" alt="logo.svg" />
+            </a>
+          </div>
+          <p className="footer__year">2022</p>
+          <a href="https://rs.school/js/">
+            <img
+              className="footer_logoRs"
+              src="../../../logoRs.svg"
+              width="100"
+              height="100"
+              alt="logoRs.svg"
+            />
           </a>
         </div>
-        <p className="footer__year">2022</p>
-        <a href="https://rs.school/js/">
-          <img
-            className="footer_logoRs"
-            src="../../../logoRs.svg"
-            width="100"
-            height="100"
-            alt="logoRs.svg"
-          />
-        </a>
       </div>
     </footer>
   );
